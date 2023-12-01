10621767cd8ea08ffc9903cee225466d0a7d4afd
erkenberg <contact@erkenberg.info>
2023-12-02 00:03:39 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,50 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return (input.map { line ->
+            ((line.find { it.isDigit() })!!.toString() + (line.last { it.isDigit() })).toInt()
+        }).sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val replacementMap = mapOf(
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9"
+        )
+        val matcher = Regex(
+            replacementMap.keys.joinToString("|")
+                    + "|"
+                    + (1..9).joinToString("|")
+        )
+
+        fun toResult(line: String): Int {
+            val firstMatch = matcher.find(line)!!.value
+            val first = if (firstMatch.length == 1) firstMatch else replacementMap[firstMatch]!!
+
+            var index = line.length - 1
+            var lastMatch: MatchResult? = null
+            while (index >= 0) {
+                lastMatch = matcher.find(line.substring(index))
+                if (lastMatch != null) break
+                index--
+            }
+            val last = if (lastMatch!!.value.length == 1) lastMatch.value else replacementMap[lastMatch.value]!!
+            return (first + last).toInt()
+        }
+
+        return (input.map { line -> toResult(line) }).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("Day01_test1")) == 142)
+    check(part2(readInput("Day01_test2")) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
